8f66a1a4b4cd08c8eba44dbe1c370a42d490a6ca
Vitor Cesario <vitorsousa676@gmail.com>
2023-08-28 17:40:37 -0300

    Aula5

---
+++ Aula5/Exercício Matriz 04/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Exercício_Matriz_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriz01 = new int[3, 3];
+            int somaprincipal = 0 , somasecundaria = 0 ;
+
+
+
+            for (int indicelinha = 0; indicelinha < matriz01.GetLength(0); indicelinha++)
+
+                for (int indicecoluna = 0; indicecoluna < matriz01.GetLength(1); indicecoluna++)
+                {
+                    Console.WriteLine($"Informe seus Números: {indicelinha} , {indicecoluna} ");
+                    matriz01[indicelinha, indicecoluna] = Convert.ToInt32(Console.ReadLine());
+
+                }
+            for (int indicelinha = 0; indicelinha < matriz01.GetLength(0); indicelinha++)
+
+                for (int indicecoluna = 0; indicecoluna < matriz01.GetLength(1); indicecoluna++)
+                {
+                    if (indicelinha == indicecoluna)
+                    {
+                       Console.WriteLine($"A diagonal principal é: {matriz01 [indicelinha , indicecoluna]}");
+                        somaprincipal += matriz01[indicelinha, indicecoluna];
+                    }
+                    
+
+                }
+                    
+
+            for (int indicelinha = 0; indicelinha < matriz01.GetLength(0); indicelinha++)
+
+                for (int indicecoluna = 0; indicecoluna < matriz01.GetLength(1); indicecoluna++)
+                {
+                    if ((indicelinha + indicecoluna) == matriz01.GetLength(0) - 1)
+
+                    {
+                       Console.WriteLine($"A diagonal secundaria é: {matriz01[indicelinha, indicecoluna]}");
+                        somasecundaria += matriz01[indicelinha, indicecoluna];
+                    }
+                    
+                   
+
+                }
+            Console.WriteLine($"O resultado da soma principal é {somaprincipal} :");
+            Console.WriteLine($"O resultado da soma secundaria é {somasecundaria} :");
+
+        }
+
+    }
+}
---
+++ Aula5/Exercício Vetores 01/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Exercício_Vetores_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor =  { 2, 5, 1, 3, 4, 9, 7, 8, 10, 6 };
+            int compara = 0;
+
+            for (int i = 0; i < vetor.Length; i ++)
+            {
+                Console.Write($"{vetor[i]}" + " ");
+            }
+
+            Console.WriteLine("\nDigite seu Número para ver sua posição ");
+            compara = Convert.ToInt32(Console.ReadLine());
+
+            if (Array.IndexOf(vetor, compara) != -1 )
+            { 
+            Console.WriteLine($"A posição de {compara} é:" + Array.IndexOf(vetor, compara));
+            }
+
+            else 
+            { 
+            Console.WriteLine($" Não temos esse numero ");
+            }
+
+            
+            
+           
+               
+           
+            
+        }
+    }
+}
